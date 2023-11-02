efd7540a4eaba4fe9b730f03accaff9eff7cc640
Chad Shaw <chad.shaw@vivint.com>
2023-11-02 17:11:54 -0600

    fix util annotations

--- util.py
+++ util.py
@@ -1,13 +1,13 @@
 from functools import reduce
 from typing import Any
 
-def get_json_key(json, *keys) -> Any:
+def get_json_key(json: dict, *keys: str) -> Any:
     o = ''
-    def getter(level, key):
+    def getter(level: dict, key: str) -> Any:
         return o if level is o else level.get(key, o)
     return reduce(getter, keys, json)
 
-def type_check(type):
+def type_check(type: str) -> (str, str):
     title = 'title'
     type = type.rstrip('s')
     if type == 'show':  
